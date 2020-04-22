be8d241d95c475f7fe970e6c5c5d4d2e34d1ae24
Florian <florianhusson@hotmail.fr>
2020-04-22 18:21:10 +0200

    Tout ça là

---
+++ RIP/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("Lol wtf l'instance du GameManager est null XD");
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    enum GameState
+    {
+        MainMenu,
+        InGame,
+        Pause,
+        GameOver
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ RIP/Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("Ca par exemple, l'instance du UI Manager est nulle :o");
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
