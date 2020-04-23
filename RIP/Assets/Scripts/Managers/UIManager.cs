408fdb7c46c719e52440ffd6aeb93860b13402a7
CroissantNinja <55735063+CroissantNinja@users.noreply.github.com>
2020-04-23 17:12:43 +0200

    Début managing UI (fail TwT)

--- RIP/Assets/Scripts/Managers/UIManager.cs
+++ RIP/Assets/Scripts/Managers/UIManager.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager instance;
+    private Player player = new Player();
+    private Text comptItems;
+    private List<GameObject[]> itemsList;
+    private GameObject[] boneList;
+    private GameObject[] zombList;
+    private BoxCollider2D boneCollider;
+
+    private int comptOs = 0;
+    private int comptZomb = 0;
+    int indexz;
+    int indexb;
+
+
+
     public static UIManager Instance
     {
         get
@@ -20,15 +35,62 @@ public class UIManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        player = GetComponent<BoxCollider2D>();
+        boneList = GameObject.FindGameObjectsWithTag("Bone");
+        zombList = GameObject.FindGameObjectsWithTag("Zomb");
+        itemsList.Add(boneList);
+        itemsList.Add(zombList);
+        comptItems = GetComponent<Text>();
     }
 
-    void Start()
+    private void Start()
     {
-        
+
     }
-    
+
     void Update()
     {
-        
+        if(player)
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+
+            if (indexb == boneList.Length)
+            {
+                return;
+            }
+            else
+            {
+                    Compteur(itemsList, zombList, boneList[indexb], ref comptOs, ref indexb);
+                indexb++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+
+            if (indexz == zombList.Length)
+            {
+                return;
+            }
+            else
+            {
+                Compteur(itemsList, zombList, zombList[indexz], ref comptZomb, ref indexz);
+                indexz++;
+            }
+        }
+
+        comptItems.text = "Os : " + comptOs.ToString() + System.Environment.NewLine + "Zomb : " + comptZomb.ToString();
+    }
+
+
+    private int Compteur(List<GameObject[]> itemsBigList, GameObject[] itemsList, GameObject items, ref int comp, ref int i)
+    {
+        itemsBigList.Remove(itemsList[items]);
+        comp++;
+        return comp;
     }
 }
