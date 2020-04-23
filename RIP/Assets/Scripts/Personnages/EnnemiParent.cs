f6b4f5015001c616a3f3902fedd8e6b6eeb804c2
QuentinLamy <quentin.lamy16@gmail.com>
2020-04-23 17:36:52 +0200

    Zombie
    attaque à fix

--- RIP/Assets/Scripts/Personnages/EnnemiParent.cs
+++ RIP/Assets/Scripts/Personnages/EnnemiParent.cs
@@ -6,15 +6,62 @@ public class EnnemiParent : MonoBehaviour
 {
     // Script parent des ennemis, pour mettre les scripts qui seront communs à tout les ennemis
 
+    [SerializeField]
+    protected int hp;
+
+    [SerializeField]
+    protected int speed;
+
+    protected bool joueurNull = true;
+
+    protected Vector2 Joueur;
+
+    protected Rigidbody2D rigid2d;
+
+    protected bool preparingAttack;
 
     void Start()
     {
-        
+        rigid2d = this.GetComponent<Rigidbody2D>();
     }
-
 
     void Update()
     {
         
+    }
+
+    protected void Movement()
+    {
+        if(joueurNull == false && preparingAttack == false)
+        {
+            Debug.Log("je bouge");
+            rigid2d.position = Vector2.MoveTowards(rigid2d.position, Joueur, speed * Time.deltaTime);
+            this.rigid2d.MovePosition(rigid2d.position);
+        }
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            joueurNull = false;
+        }
+    }
+    protected void OnTriggerStay2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if(player != null)
+        {
+            Joueur = player.gameObject.GetComponent<Transform>().position;
+        }
+    }
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            joueurNull = true;
+        }
     }
 }
---
+++ RIP/Assets/Scripts/Personnages/EnnemiZombie.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiZombie : EnnemiParent
+{
+    private BoxCollider2D box2d;
+
+    private float dashAttackTimer = 0;
+
+    void Start()
+    {
+        hp = 2;
+        speed = 2;
+        rigid2d = this.GetComponent<Rigidbody2D>();
+        box2d = this.GetComponent<BoxCollider2D>();
+        preparingAttack = false;
+    }
+
+    void Update()
+    {
+        Attack();
+
+        if(preparingAttack == true)
+        {
+            dashAttackTimer += Time.deltaTime;
+            if(dashAttackTimer > 3)
+            {
+                Debug.Log("je touche !");
+                preparingAttack = false;
+                this.box2d.enabled = false;
+                dashAttackTimer = 0;
+            }
+        }
+        //essai de tourner la hitbox d'attaque du zombie pour regarder le joeuur (ne fonctionne pas)
+        //this.box2d.transform.Rotate(0, 0, Vector2.Angle(this.rigid2d.position, Joueur));
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Attack()
+    {
+        if (Joueur.x - this.rigid2d.position.x > -1.5 && Joueur.x - this.rigid2d.position.x < 1.5 &&
+            Joueur.y - this.rigid2d.position.y > -1.5 && Joueur.y - this.rigid2d.position.y < 1.5 && joueurNull == false && preparingAttack == false)
+        {
+            preparingAttack = true;
+            Debug.Log("à la fin de l'envoi...");
+            this.box2d.enabled = true;
+        }
+    }
+}
