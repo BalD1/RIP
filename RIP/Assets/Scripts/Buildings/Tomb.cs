f1c40888d161a75a7750d3937f724ee909a1ad28
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2020-05-05 10:44:21 +0200

    Tombe ! :skull:

---
+++ RIP/Assets/Scripts/Buildings/Tomb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tomb : MonoBehaviour
+{
+    public GameObject Ennemy;
+    private Vector2 tombPos;
+    private bool Spawned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tombPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.SendGameTime() == GameManager.GameTime.Night && Spawned == false)
+        {
+            Instantiate(Ennemy, tombPos, Quaternion.identity);
+            Spawned = true;
+        }
+        if (GameManager.Instance.SendGameTime() == GameManager.GameTime.Day)
+        {
+            Spawned = false;
+        }
+    }
+}
