f54ed0cee3c5aae3a9d0826a03add7bd91b76e22
QuentinLamy <quentin.lamy16@gmail.com>
2020-05-07 17:26:28 +0200

    oune peu plus de lumière

---
+++ RIP/Assets/Scripts/Lumières/NightLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightLight : MonoBehaviour
+{
+    private Light nightLight;
+
+    void Start()
+    {
+        nightLight = this.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if(GameManager.Instance.SendGameTime() == GameManager.GameTime.Day)
+        {
+            this.nightLight.intensity = 0;
+        }
+        else
+        {
+            this.nightLight.intensity = 10;
+        }
+    }
+}
---
+++ RIP/Assets/Scripts/Lumières/SunLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunLight : MonoBehaviour
+{
+    private Light sunLight;
+
+    void Start()
+    {
+        sunLight = this.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            this.sunLight.intensity += 0.5f;
+        }
+        else if(Input.GetKeyDown(KeyCode.O))
+        {
+            this.sunLight.intensity -= 0.5f;
+        }
+    }
+}
