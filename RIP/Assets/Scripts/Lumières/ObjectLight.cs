f839e3e8b2f932e573352c64824c89d93db50bd6
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2020-05-26 17:18:38 +0200

    Lumiere ! :flashlight:

---
+++ RIP/Assets/Scripts/Lumières/ObjectLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+
+public class ObjectLight : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D Light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Light = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.SendGameTime() == GameManager.GameTime.Day && Light.intensity > 0)
+        {
+            Light.intensity -= 0.0040f;
+        }
+        else if (GameManager.Instance.SendGameTime() == GameManager.GameTime.Night && Light.intensity < 1)
+        {
+            Light.intensity += 0.0040f;
+        }
+    }
+}
